07674e69d3a1f1f81942fcbd2729e92858f2202b
7Saksham7 <132772217+7Saksham7@users.noreply.github.com>
2023-05-08 18:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+import joblib
+import os
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("home.html")
 
-@app.route('/predict_sales', methods=['POST'])
-def predict_sales():
-    # Get input values
-    item_weight = float(request.form['item_weight'])
-    item_fat_content = request.form['item_fat_content']
-    item_visibility = float(request.form['item_visibility'])
-    item_type = request.form['item_type']
-    
-    # Call machine learning model to predict sales
-    # ...
-    predicted_sales = 10000
-    
-    # Display predicted sales
-    result = "Predicted sales for {} with {} fat content, {} weight, and {} visibility is {}".format(item_type, item_fat_content, item_weight, item_visibility, predicted_sales)
-    return render_template('index.html', result=result)
+@app.route('/predict', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        item_weight = float(request.form['item_weight'])
+        item_fat_content = float(request.form['item_fat_content'])
+        item_visibility = float(request.form['item_visibility'])
+        item_type = float(request.form['item_type'])
+        item_mrp = float(request.form['item_mrp'])
+        outlet_establishment_year = float(request.form['outlet_establishment_year'])
+        outlet_size = float(request.form['outlet_size'])
+        outlet_location_type = float(request.form['outlet_location_type'])
+        outlet_type = float(request.form['outlet_type'])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+        X = np.array([[item_weight, item_fat_content, item_visibility, item_type, item_mrp,
+                       outlet_establishment_year, outlet_size, outlet_location_type, outlet_type]])
+
+        scaler_path = r'C:\Users\SAKSHAM RAJVANSHI\Desktop\ml\models\sc.sav'
+        sc = joblib.load(scaler_path)
+        X_std = sc.transform(X)
+
+        model_path = r'C:\Users\SAKSHAM RAJVANSHI\Desktop\ml\models\lr.sav'
+        model = joblib.load(model_path)
+
+        Y_pred = model.predict(X_std)
+
+        return jsonify({'Prediction': str(Y_pred[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=9457)
+    
